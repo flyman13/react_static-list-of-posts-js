545760e515258136c6b92851e554fe528e7fefd9
Artem <muha13artem@gmail.com>
2026-04-04 10:23:28 +0300

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.scss';
 
-// import postsFromServer from './api/posts.json';
-// import commentsFromServer from './api/comments.json';
-// import usersFromServer from './api/users.json';
+//import postsFromServer from './api/posts.json';
+//import commentsFromServer from './api/comments.json';
+//import usersFromServer from './api/users.json';
 
 export const App = () => (
   <section className="App">
